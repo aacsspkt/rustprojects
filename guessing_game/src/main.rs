c19f440d3d9653fada05fd5f53d458058649d07b
Ashish Sapkota <aacsspkt3@gmail.com>
2023-09-02 00:15:49 +0545

    number guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,39 @@
 use std::io;
+use rand::{Rng, thread_rng};
+
 fn main() {
-    println!("Welcome to guessing game.");
-    println!("Please enter your guess:");
+    
+    println!("Welcome to Number guessing game.");
+    
+    let mut rng = thread_rng();
+    let num : u32 = rng.gen_range(0..101);
+    
+    let mut tries = 0;
+    // println!("Random num: {}", num);
+    loop {
+        println!("Please enter your guess between (1-100):");
+        
+        tries+=1;
 
-    let mut guess = String::new();
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read line");
+        
+        let guess = guess.trim();
+        if guess.len() <= 0 {
+            continue;
+        }
+        let guess = guess.parse::<u32>().expect("Invalid Number");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Could not read line");
-
-    println!("Your guess was {}", guess);
+        if guess > num {
+            println!("Too high!");
+        } 
+        else if guess < num {
+            println!("Too low!");
+        } else {
+            println!("CORRECT! after tries: {}", tries);
+            break;
+        }
+    }
 }
