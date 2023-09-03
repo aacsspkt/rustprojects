059ca0245a346b54db9aca409c672ed30774d1d4
Ashish Sapkota <aacsspkt3@gmail.com>
2023-09-03 22:43:15 +0545

    optimize function

--- largest_element_in_array/src/lib.rs
+++ largest_element_in_array/src/lib.rs
@@ -1,12 +1,19 @@
-pub fn get_largest_element<T: PartialOrd + Clone>(elements: &[T]) -> T {
+use std::cmp::Ordering;
+
+pub fn get_largest_element<T: PartialOrd + Clone>(elements: &[T]) -> Option<T> {
+    if elements.is_empty() {
+        return None; // Handle the case of an empty slice
+    }
+
     let mut largest = elements[0].clone();
-    for element in elements {
-        if element > &largest {
-            largest = (*element).clone();
+
+    for element in elements.iter().skip(1) {
+        if let Ordering::Greater = element.partial_cmp(&largest).unwrap_or(Ordering::Equal) {
+            largest = element.clone();
         }
     }
 
-    return largest;
+    return Some(largest);
 }
 
 #[cfg(test)]
@@ -15,12 +22,16 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let list1 = [2, 3, 9, 10, 30, 23, 32, ];
+        let list1 = [2, 3, 9, 10, 30, 23, 32];
         let result = get_largest_element(&list1);
-        assert_eq!(result, 32);
-
+        assert_eq!(result, Some(32));
+        
         let list2 = ['A', 'B', 'C', 'D', 'F'];
         let result = get_largest_element(&list2);
-        assert_eq!(result, 'F');
+        assert_eq!(result, Some('F'));
+        
+        let list3: [i32; 0] = [];
+        let result = get_largest_element(&list3);
+        assert_eq!(result, None);
     }
 }
