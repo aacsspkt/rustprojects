d84a3122c7b4c7aae25b4f4fe4250b41be9b90e3
Ashish Sapkota <aacsspkt3@gmail.com>
2023-09-08 11:53:48 +0545

    return vector from series instead of printing

--- fibonacci_series/src/main.rs
+++ fibonacci_series/src/main.rs
@@ -1,23 +1,29 @@
 fn main() {
     println!("Hello, world!");
 
-    fibonacci_series(100);
+    let series = fibonacci_series(100);
+    for i in series {
+        println!("{i} ");
+    }
 }
 
-fn fibonacci_series(num: u32) {
+fn fibonacci_series(num: u32) -> Vec<u128> {
     let mut a: u128 = 0;
     let mut b: u128 = 1;
 
     let mut sum = a + b;
     let mut count = 1;
 
+    let mut series = Vec::new();
     while count <= num {
         count += 1;
 
-        println!("{a}");
+        series.push(a);
 
         a = b;
         b = sum;
         sum = a + b;
     }
+
+    series
 }
