26ce495bb538d4531e5570032d4d715a0c44eea9
Ashish Sapkota <aacsspkt3@gmail.com>
2023-09-08 12:23:09 +0545

    use cache to optimize fibonacci series

--- fibonacci_series_using_recursion/src/main.rs
+++ fibonacci_series_using_recursion/src/main.rs
@@ -1,19 +1,28 @@
+use std::collections::HashMap;
+
 fn main() {
     // computation becomes slow after 40 iteration!
-    let n = 40;
-
+    let n = 100;
+    let mut memo:HashMap<u128, u128> = HashMap::new();
+    
     println!("Fibonacci series:");
     for i in 1..=n {
-        println!("F({}) {}",i, fibonacci_series(i));
+        println!("F({}) {}", i, fibonacci_series_memoized(i, &mut memo));
     }
 }
 
-fn fibonacci_series(n: u64) -> u128 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
-    } else {
-        return fibonacci_series(n - 1) + fibonacci_series(n - 2)
+fn fibonacci_series_memoized(n: u128, memo: &mut HashMap<u128, u128>) -> u128 {
+    if n <= 1 {
+        return n;
     }
+
+    if let Some(&result) = memo.get(&n) {
+        return result;
+    }
+
+    let result = fibonacci_series_memoized(n - 1, memo) + fibonacci_series_memoized(n - 2, memo);
+
+    memo.insert(n, result);
+
+    result
 }
